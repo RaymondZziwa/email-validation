17b7ce17ac36d2fa451c3b6087bcdf37e06d26cd
raywebgenius <46450094+fabricray256@users.noreply.github.com>
2020-12-11 10:32:01 +0300

    email input by user is validated

---
+++ log in .js
@@ -0,0 +1,19 @@
+// JavaScript Document
+function submit(){
+	var correct = document.getElementById("input1");
+
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(correct.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.correct.focus();
+return false;
+}
+}
+
